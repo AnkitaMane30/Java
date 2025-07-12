1ed5d5f08feeb7a79c43323552e63426f38c8a89
AnkitaMane30 <maneankita831@gmail.com>
2025-07-12 08:50:13 +0530

    Add files via upload

---
+++ OOP_classes/Demo3.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+/*class Date{
+	private int day;
+	private int month;
+	private int year;
+	
+	public Date()
+	{
+		Calendar c=Calendar.getInstance();
+		day=c.get(Calendar.DATE);
+		month=c.get(Calendar.MONTH)+1;
+		year=c.get(Calendar.YEAR);
+	}
+	
+	public void printRecord()
+	{
+		System.out.println("Day:"+this.day);
+		System.out.println("Month:"+this.month);
+		System.out.println("Year:"+this.year);
+	}
+	
+	public void acceptRecord()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter date:");
+		day=sc.nextInt();
+		System.out.println("Enter Month:");
+		month=sc.nextInt();
+		System.out.println("Enter year");
+		year=sc.nextInt();
+	}
+}
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d=new Date();
+		d.printRecord();
+		d.acceptRecord();
+		d.printRecord();
+	}
+
+}*/
