29aca745afe2ce3fe060b6a1f2417f93f2a7cc6d
AnkitaMane30 <maneankita831@gmail.com>
2025-08-01 23:08:12 +0530

    Add files via upload

---
+++ ComaparableUsingGenerics.java
@@ -0,0 +1,40 @@
+package com.dkte;
+
+class Fraction implements Comparable<Fraction>
+{
+	private int n,d;
+	public Fraction()
+	{
+		
+	}
+	public Fraction(int n,int d)
+	{
+		this.n=n;
+		this.d=d;
+	}
+	public double value()
+	{
+		double r=n/d;
+		return r;
+	}
+	@Override
+	public int compareTo(Fraction other)
+	{	
+		if(this.value()==other.value())
+			return 0;
+		else if(this.value()>other.value())
+			return 1;
+		else
+			return -1;
+	}
+}
+public class ComparableUsingGenerics {
+
+	public static void main(String[] args) {
+		Fraction f1= new Fraction(10,2);
+		Fraction f2=new Fraction(20,2);
+		int r=f1.compareTo(f2);
+		System.out.println("Res:"+r);
+	}
+
+}
